a4a0e5def7417d66939846e2cda15e99799d2945
Bence27 <bence2002327@gmail.com>
2023-09-21 11:19:34 +0200

    hibak_kiemelve

--- main.cpp
+++ main.cpp
@@ -4,21 +4,21 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    int *b = new int[NELEMENTS];
-    std::cout << '1-100 ertekek duplazasa'
-    for (int i = 0;)
+    int* b = new int[NELEMENTS]; //hibás változónév NELEMENTS nincs definiálva
+    std::cout << '1-100 ertekek duplazasa' //; hiány és '' helyett "" mivel ez string
+        for (int i = 0;) //Hiányos forciklus
+        {
+            b[i] = i * 2;
+        }
+    for (int i = 0; i; i++) //Hiányos forciklus
     {
-        b[i] = i * 2;
+        std::cout << "Ertek:" // nincs ;
     }
-    for (int i = 0; i; i++)
-    {
-        std::cout << "Ertek:"
-    }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    int atlag;
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag; //atlagot nem előnyös int ként definiálni és nincs daklaráció: atlag=0
+    for (int i = 0; i < N_ELEMENTS, i++) //for ciklus syntaxis hibás
     {
-        atlag += b[i]
+        atlag += b[i] // nincs ;
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
